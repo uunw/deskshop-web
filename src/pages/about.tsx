27ad6237d53b4edcd570e200a740cad4c6206749
uunw <taokaeg2@gmail.com>
2023-01-31 10:37:40 +0700

    docs(abuot-readme): add github url, and readme

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,3 +1,5 @@
+import NextLink from 'next/link';
+
 import type { NextPageWithLayout } from './_app';
 
 const AboutPage: NextPageWithLayout = () => {
@@ -6,6 +8,17 @@ const AboutPage: NextPageWithLayout = () => {
       <div>
         <p>hello my name is nonthaphan instahwong</p>
         <p>this is my ecommerce for learning project</p>
+
+        <p className="mt-5">
+          project repository{` `}
+          <NextLink
+            href="https://github.com/uunw/deskshop-web"
+            target="_blank"
+            className="font-bold text-blue-400"
+          >
+            github
+          </NextLink>
+        </p>
       </div>
     </>
   );
